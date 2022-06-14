35009ac85f0d0b0f09fa92206ad05db1aabef188
beyoutoday <github.mail2116@gmail.com>
2022-06-14 08:47:51 -0700

    imports/assets and display created

---
+++ main.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+import fnmatch 
+import os
+from pygame import mixer
+
+display = tk.Tk()
+display.title("Music Player")
+display.geometry("500x500")
+display.config(bg = 'black')
+
+display.mainloop()
